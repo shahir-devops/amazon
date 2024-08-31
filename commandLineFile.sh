6768db2617159615b58cf434eeaffd55bb200361
shahir-syed <shahirsyeed@gmail.com>
2024-08-31 10:41:13 +0000

    commandLineFile

---
+++ commandLineFile.sh
@@ -0,0 +1,11 @@
+echo "your file name is: $0"
+echo "your first argument passed: $1"
+echo "your secound argument passed: $2"
+echo "your third arguments passed: $3"
+echo "total number is: $#"
+echo "total string takes as single argument: $*"
+echo "each word in the string takes as single word : $@ "
+date
+echo "file is exectued or not : $?"
+echo "process id $$"
+
