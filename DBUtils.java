bdcab3ce441e14650595f279f25e686c660eb35e
shahir-syed <shahirsyeed@gmail.com>
2024-09-03 11:02:53 +0000

    staged

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,3 @@
 This is jaava db file
 this command is from master branch 
-
+this command is from development branch 
--- Deploy.java
+++ Deploy.java
@@ -1,2 +1,3 @@
 this is java file 
 i am from mithun technologies
+i am writing this from development branch
